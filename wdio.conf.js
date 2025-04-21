4392c12b0f54f99e3d58a743d7ce0f9c5ff326c6
Diego <droxo63@gmail.com>
2025-04-21 13:11:31 -0300

    final update

--- wdio.conf.js
+++ wdio.conf.js
@@ -27,40 +27,29 @@ exports.config = {
     exclude: [
         // 'path/to/excluded/files'
     ],
-    //
-    // ============
-    // Capabilities
-    // ============
-    // Define your capabilities here. WebdriverIO can run multiple capabilities at the same
-    // time. Depending on the number of capabilities, WebdriverIO launches several test
-    // sessions. Within your capabilities you can overwrite the spec and exclude options in
-    // order to group specific specs to a specific capability.
-    //
-    // First, you can define how many instances should be started at the same time. Let's
-    // say you have 3 different capabilities (Chrome, Firefox, and Safari) and you have
-    // set maxInstances to 1; wdio will spawn 3 processes. Therefore, if you have 10 spec
-    // files and you set maxInstances to 10, all spec files will get tested at the same time
-    // and 30 processes will get spawned. The property handles how many capabilities
-    // from the same test should run tests.
+   
     //
     maxInstances: 10,
-    //
-    // If you have trouble getting all important capabilities together, check out the
-    // Sauce Labs platform configurator - a great tool to configure your capabilities:
-    // https://saucelabs.com/platform/platform-configurator
-    //
+  
+    
     capabilities: [{
     
-        // maxInstances can get overwritten per capability. So if you have an in-house Selenium
-        // grid with only 5 firefox instances available you can make sure that not more than
-        // 5 instances get started at a time.
+
         maxInstances: 5,
-        //
+        
         browserName: 'chrome',
 
-        'goog:chromeOptions': {
-            args: ['--headless', '--disable-gpu'] 
-        },
+       'goog:chromeOptions': {
+  args: [
+    '--headless',
+    '--no-sandbox',
+    '--disable-dev-shm-usage',
+    '--disable-gpu',
+    '--disable-extensions',
+    '--window-size=1920,1080'
+  ]
+}
+
 
         acceptInsecureCerts: true
         // If outputDir is provided WebdriverIO can capture driver session logs
@@ -115,7 +104,14 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['selenium-standalone'],
+    services: ['chromedriver'],
+    capabilities: [{
+        maxInstances: 1,
+        browserName: 'chrome',
+        acceptInsecureCerts: true, // Permite trabalhar com sites sem certificado SSL válido
+      }],
+      
+
 
     
     // Framework you want to run your specs with.
